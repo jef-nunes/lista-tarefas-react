c81a18bc77d91313e82fa90a9d67053ba87a863e
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-06-07 15:37:08 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TarefasComponente from './components/TarefasComponente';
+
+function App() {
+  return (
+    <>
+    <TarefasComponente/>
+    </>
+  )
+}
+
+export default App;
